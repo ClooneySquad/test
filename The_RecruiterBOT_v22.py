ae72edb6e674e76b6fdcb8faf0f09be71a08d552
ClooneySquad <clooney.squad@internet.ru>
2024-07-11 20:57:06 +0300

    Update The_RecruiterBOT_v22.py

--- The_RecruiterBOT_v22.py
+++ The_RecruiterBOT_v22.py
@@ -4,8 +4,15 @@ bot = telebot.TeleBot('TOKEN')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    photo_url = 'https://github.com/ClooneySquad/test/blob/main/photo_clooney.jpg?raw=true'
-    bot.send_photo(message.chat.id, photo_url)
-    bot.send_message(message.chat.id, "Привет! Я отправил тебе изображение.")
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
+    itembtn = telebot.types.KeyboardButton('Поздароваться')
+    markup.add(itembtn)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if message.text == 'Поздароваться':
+        photo_url = 'https://github.com/ClooneySquad/test/blob/main/photo_clooney.jpg?raw=true'
+        bot.send_photo(message.chat.id, photo_url)
 
 bot.polling()
