c742390c3e45002e48167e518b1a18320859dcf2
ClooneySquad <clooney.squad@internet.ru>
2024-07-12 23:07:22 +0300

    Update The_RecruiterBOT_v22.py

--- The_RecruiterBOT_v22.py
+++ The_RecruiterBOT_v22.py
@@ -8,7 +8,9 @@ def main_menu(message):
     itembtn2 = telebot.types.KeyboardButton('Текст 2')
     itembtn3 = telebot.types.KeyboardButton('Вакансии')
     markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat.id, "Привет! Нажми на одну из кнопок:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Привет, {}! Это приветственный текст".format(message.from_user.first_name), reply_markup=markup)
+    photo_url = 'https://github.com/ClooneySquad/test/blob/main/test3.jpg?raw=true'
+    bot.send_photo(message.chat.id, photo_url)
 
 def vacancies_menu(message):
     markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
@@ -34,12 +36,15 @@ def echo_all(message):
     elif message.text == 'Вакансия 1':
         photo_url = 'https://github.com/ClooneySquad/test/blob/main/test3.jpg?raw=true'
         bot.send_photo(message.chat.id, photo_url)
+        bot.send_message(message.chat.id, "Текст для Вакансии 1")
     elif message.text == 'Вакансия 2':
         photo_url = 'https://github.com/ClooneySquad/test/blob/main/testtesttest.jpg?raw=true'
         bot.send_photo(message.chat.id, photo_url)
+        bot.send_message(message.chat.id, "Текст для Вакансии 2")
     elif message.text == 'Вакансия 3':
         photo_url = 'https://github.com/ClooneySquad/test/blob/main/test2.jpg?raw=true'
         bot.send_photo(message.chat.id, photo_url)
+        bot.send_message(message.chat.id, "Текст для Вакансии 3")
     elif message.text == 'Назад':
         main_menu(message)
 
