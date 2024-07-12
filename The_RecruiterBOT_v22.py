307b508dc5ef961e9bb0a5f707af899016cf3d47
ClooneySquad <clooney.squad@internet.ru>
2024-07-12 22:38:12 +0300

    Update The_RecruiterBOT_v22.py

--- The_RecruiterBOT_v22.py
+++ The_RecruiterBOT_v22.py
@@ -2,18 +2,36 @@ import telebot
 
 bot = telebot.TeleBot('7358013319:AAFae4MKwf2dryKTiG9CmHybBHmAofjd_UY')
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
+def main_menu(message):
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = telebot.types.KeyboardButton('Текст 1')
+    itembtn2 = telebot.types.KeyboardButton('Текст 2')
+    itembtn3 = telebot.types.KeyboardButton('Вакансии')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat.id, "Привет! Нажми на одну из кнопок:", reply_markup=markup)
+
+def vacancies_menu(message):
     markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
     itembtn1 = telebot.types.KeyboardButton('Вакансия 1')
     itembtn2 = telebot.types.KeyboardButton('Вакансия 2')
     itembtn3 = telebot.types.KeyboardButton('Вакансия 3')
-    markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat.id, "Привет! Нажми на одну из кнопок:", reply_markup=markup)
+    itembtn4 = telebot.types.KeyboardButton('Назад')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    bot.send_message(message.chat.id, "Выбери вакансию или вернись назад:", reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    main_menu(message)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    if message.text == 'Вакансия 1':
+    if message.text == 'Текст 1':
+        bot.send_message(message.chat.id, "Это первый текст")
+    elif message.text == 'Текст 2':
+        bot.send_message(message.chat.id, "Это второй текст")
+    elif message.text == 'Вакансии':
+        vacancies_menu(message)
+    elif message.text == 'Вакансия 1':
         photo_url = 'https://github.com/ClooneySquad/test/blob/main/test3.jpg?raw=true'
         bot.send_photo(message.chat.id, photo_url)
     elif message.text == 'Вакансия 2':
@@ -22,5 +40,7 @@ def echo_all(message):
     elif message.text == 'Вакансия 3':
         photo_url = 'https://github.com/ClooneySquad/test/blob/main/test2.jpg?raw=true'
         bot.send_photo(message.chat.id, photo_url)
+    elif message.text == 'Назад':
+        main_menu(message)
 
 bot.polling()
