cb33ee68cc93b20eafc43561e5922ab790bfbaf6
ClooneySquad <clooney.squad@internet.ru>
2024-07-14 10:52:15 +0300

    Update The_RecruiterBOT_v22.py

--- The_RecruiterBOT_v22.py
+++ The_RecruiterBOT_v22.py
@@ -18,7 +18,8 @@ def vacancies_menu(message):
     itembtn2 = telebot.types.KeyboardButton('Водитель')
     itembtn3 = telebot.types.KeyboardButton('Штурмовик')
     itembtn4 = telebot.types.KeyboardButton('Назад')
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    itembtn5 = telebot.types.KeyboardButton('Медик')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn5, itembtn4)
     bot.send_message(message.chat.id, "Выбери вакансию или вернись назад:", reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
@@ -28,7 +29,7 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     if message.text == 'Про отряд':
-        bot.send_message(message.chat.id, """Легендарный отряд «КЛУНИ» (ранее отряд «ЛЕВ») объявляет набор бойцов для участия в специальной военной операции. Отряд носит имя своего первого командира с позывным «КЛУНИ», который погиб в марте 2022 г., защищая свою родную землю. Отряд был создан 28 октября 2021 года и в настоящий момент выполняет боевые задачи совместно с Вооруженными Силами Российской Федерации. 
+        bot.send_message(message.chat.id, """Легендарный отряд «КЛУНИ» (ранее отряд «ЛЕВ») объявляет набор бойцов для участия в специальной военной операции. Отряд носит имя своего первого командира с позывным «КЛУНИ», который погиб в марте 2022 г., защищая свою родную землю. Отряд был создан с начала СВО и в настоящий момент выполняет боевые задачи совместно с Вооруженными Силами Российской Федерации. 
 С начала СВО личный состав отряда удостоен следующими государственными наградами: 
 1.	Герой ДНР – 1
 2.	Орден Мужества – 54
@@ -42,7 +43,7 @@ def echo_all(message):
 На службу также принимаются иностранные граждане. Для иностранцев существует возможность оформить гражданство Российской Федерации.
 """) #
     elif message.text == 'Контакты':
-        bot.send_message(message.chat.id, """По всем вопросам: @ClooneyRecruiter \n+79495660687 (только для сообщений), \nНаша 'Отчетная' группа в телеграмм: https://t.me/ClooneySquad""") #https://t.me/@ClooneyRecruiter
+        bot.send_message(message.chat.id, """По всем вопросам: @ClooneyRecruiter \n+79495660687 (только для сообщений в телеграмм), \nНаша 'Отчетная' группа в телеграмм: https://t.me/ClooneySquad""") #https://t.me/@ClooneyRecruiter
     elif message.text == 'Вакансии':
         vacancies_menu(message)
     elif message.text == 'Оператор дрона':
@@ -63,6 +64,11 @@ def echo_all(message):
         bot.send_message(message.chat.id, """В отряд «Клуни» требуются специалисты по категории «Штурмовик». Если судьба России для тебя имеет значение, и ты готов встать в строй героев-защитников своей Родины, то отряд «Клуни» с радостью примет тебя в свои боевые ряды. 
 Если ты не имеешь опыта боевых действий, то наши инструктора в процессе обучения поделятся своими знаниями с каждым желающим и передадут все свои навыки, которыми обладают сами. 
 Каждый боец отряд «Клуни» с оружием в руках отстаивает право наших предков на землю Донбасса и Малороссии. Это наша земля и м ы готовы за неё бороться.
+""")
+    elif message.text == 'Медик':
+        photo_url = 'https://github.com/ClooneySquad/test/blob/1d7155ca09993fca84671531d491ac638e7e27bf/ScriptData/SkriptMedia/Asshole.jpg?raw=true'#'https://github.com/ClooneySquad/test/blob/main/test2.jpg?raw=true'
+        bot.send_photo(message.chat.id, photo_url)
+        bot.send_message(message.chat.id, """В Отряд 'Клуни' требуется медик \n(в настоящее время данная вакансия находится в редактировании)
 """)
     elif message.text == 'Назад':
         main_menu(message)
